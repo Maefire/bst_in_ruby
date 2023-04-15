9537d078cd0482342fad6c650442dcad65b0db1e
Maefire <masked_phoenix@yahoo.com>
2023-04-15 07:43:38 -0500

    Implement driver script
    when main.rb is run, it will automatically run the method that goes
    through an example tree, including adding some nodes, checking balance,
    rebalancing,etc...

--- main.rb
+++ main.rb
@@ -1,3 +1,27 @@
-require_relative "/lib/tree"
-require_relative "/lib/node"
-require_relative "/lib/display"
+require_relative "lib/tree"
+require_relative "lib/node"
+require_relative "lib/display"
+
+def driver
+  arr = (Array.new(15) { rand(1..100) })
+  tree_1 = Tree.new
+  tree_1.build_tree(arr)
+  p tree_1.balanced?
+  puts "level order: \t#{tree_1.level_order}\n" \
+    "preorder: \t#{tree_1.preorder}\n" \
+    "postorder: \t#{tree_1.postorder}\n" \
+    "inorder: \t#{tree_1.inorder}\n\n"
+  tree_1.pretty_print
+  tree_1.insert(101)
+  tree_1.insert(1121)
+  tree_1.insert(11023)
+  p tree_1.balanced?
+  tree_1.rebalance
+  p tree_1.balanced?
+  puts "level order: \t#{tree_1.level_order}\n" \
+  "preorder: \t#{tree_1.preorder}\n" \
+  "postorder: \t#{tree_1.postorder}\n" \
+  "inorder: \t#{tree_1.inorder}\n\n"
+  tree_1.pretty_print
+end
+driver
